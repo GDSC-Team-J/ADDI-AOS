c8fedfa59b6fc4fccfb349a199fdb691ef52721e
이빵주 <yungju9982@naver.com>
2024-01-12 22:30:56 +0900

    feat: add stroke in record button

--- app/src/main/java/com/gdsc/addi/ui/component/RecordButton.kt
+++ app/src/main/java/com/gdsc/addi/ui/component/RecordButton.kt
@@ -1,5 +1,6 @@
 package com.gdsc.addi.ui.component
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.shape.CircleShape
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gdsc.addi.ui.theme.AddiDesignSystem
 
 @Composable
@@ -26,6 +28,7 @@ fun RecordButton(
     content: @Composable RowScope.() -> Unit
 ) = Button(
     modifier = modifier.aspectRatio(1f),
+    border = BorderStroke(3.dp, AddiDesignSystem.colors.grayBorder),
     colors = buttonColors,
     shape = shape,
     onClick = onClick
